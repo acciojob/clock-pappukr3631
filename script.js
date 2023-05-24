a3215c6340baa5f230c13ec78bc44f2fe89eea97
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-05-24 15:36:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+let amPm = "";
+function hour(h) {
+    if(h<12) {
+        amPm = "AM";
+        return h;
+    }
+    if(h==12) {
+        amPm = "PM";
+        return 12;
+    }
+    amPm = "PM";
+
+    h = h - 12;
+    return h;
+}
+
+function timer() {
+    let timer = document.getElementById("timer");
+    let d = new Date();
+
+    let date = `${d.getMonth()}/${d.getDate()}/${d.getFullYear()}, `;
+
+    let time = `${hour(d.getHours())}:${d.getMinutes()}:${d.getSeconds()} ${amPm}`;
+
+    timer.innerText = date+time;
+}
+
+setInterval(timer,1000);
